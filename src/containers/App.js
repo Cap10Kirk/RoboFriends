c0c85b48ecc45a809dfb10b90d608d3838313c81
Quiara <qrkirkland@gmail.com>
2019-08-06 16:59:29 -0400

    code cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,7 +28,7 @@ class App extends Component {
 	  const filteredRobots = robots.filter(robot =>{
 		return robot.name.toLowerCase().includes(searchfield.toLowerCase())	
 		})
-		if (this.state.robots.length === 0) {
+		if (robots.length === 0) {
 			   return <h1>Loading...</h1> 
 		} else {
 		  return (
