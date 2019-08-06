8b9ba6e6e807c5253922df2512b4e094c03a6ffb
Quiara <qrkirkland@gmail.com>
2019-08-06 16:36:48 -0400

    update if statement

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,17 +28,19 @@ class App extends Component {
 	  const filteredRobots = robots.filter(robot =>{
 		return robot.name.toLowerCase().includes(searchfield.toLowerCase())	
 		})
-	   return !robots.length ?
-	  	<h1>Loading</h1> :
-	     (
-		   <div className='tc'>
-			 <h1 className='f1'>RoboFriends</h1>
-			 <SearchBox searchChange={this.onSearchChange}/>
-			 <Scroll>
+		if (this.state.robots.length === 0) {
+			   return <h1>Loading</h1> 
+		} else {
+		  return (
+		   	<div className='tc'>
+			  <h1 className='f1'>RoboFriends</h1>
+			  <SearchBox searchChange={this.onSearchChange}/>
+			  <Scroll>
 			    <CardList robots={filteredRobots}/>
-			 </Scroll>
-		   </div>	
-	    );
+			  </Scroll>
+		    </div>	
+		);
+	 }	
    }
  }
 export default App;
