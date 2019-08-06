5567b5c5dd1bbb92d375f28e08ea57c6a76c6e32
Quiara <qrkirkland@gmail.com>
2019-08-06 16:38:26 -0400

    update if statement

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 		return robot.name.toLowerCase().includes(searchfield.toLowerCase())	
 		})
 		if (this.state.robots.length === 0) {
-			   return <h1>Loading</h1> 
+			   return <h1>Loading...</h1> 
 		} else {
 		  return (
 		   	<div className='tc'>
